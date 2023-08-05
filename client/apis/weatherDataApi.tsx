5a7993cdd7068ea66c181da6a7635ab6d688a13d
humaidiar <humaidiar123@gmail.com>
2023-08-05 21:50:36 +1200

    updated

--- client/apis/weatherDataApi.tsx
+++ client/apis/weatherDataApi.tsx
@@ -7,7 +7,6 @@ export function getWeather(city: string) {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_KEY}&lang=en&units=metric`
     )
     .then((res) => {
-      console.log(res.body)
       const WeatherData: Weather = {
         name: res.body.name,
         main: res.body.weather[0].main,
@@ -20,4 +19,14 @@ export function getWeather(city: string) {
       return WeatherData
     })
     .catch((err) => console.log('Err message:' + err))
+}
+
+export function getGeoApi(value: string) {
+  return request
+    .get(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=3&appid=${
+        process.env.WEATHER_KEY
+      }&lang=en`
+    )
+    .then((res) => res.body)
 }
